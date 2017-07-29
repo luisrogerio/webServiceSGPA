9d62d9f1411d1f8485013a36b9bdb8de3017badc
Luís Rogério Ventura Martins Filho <luisrogeriojf@hotmail.com>
2017-07-29 11:35:36 -0300

    Staged

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,23 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::prefix('planoDeAula')->group(function() {
+	Route::get('/getAll', 'PlanosDeAulaController@getAll');
+	Route::post('/save', 'PlanosDeAulaController@adicionar');
+	Route::post('/edit/{id}', 'PlanosDeAulaController@editar');
+	Route::post('/show/{id}', 'PlanosDeAulaController@visualizar');
+});
+
+Route::prefix('momento')->group(function() {
+	Route::get('/getAll', 'MomentosController@getAll');
+	Route::post('/save/{planoDeAulaId}', 'MomentosController@adicionar');
+	Route::post('/edit/{id}', 'MomentosController@editar');
+});
+
+Route::prefix('recurso')->group(function() {
+	Route::get('/getAll', 'RecursosController@getAll');
+	Route::post('/save', 'RecursosController@adicionar');
+	Route::post('/edit/{id}', 'RecursosController@editar');
+});
--- routes/web.php
+++ routes/web.php
@@ -16,22 +16,3 @@ Route::get('/', function () {
 			'msg' => 'teste'
 		], 200);
 });
-
-Route::prefix('planoDeAula')->group(function() {
-	Route::get('/getAll', 'PlanosDeAulaController@getAll');
-	Route::post('/save', 'PlanosDeAulaController@adicionar');
-	Route::post('/edit/{id}', 'PlanosDeAulaController@editar');
-	Route::post('/show/{id}', 'PlanosDeAulaController@visualizar');
-});
-
-Route::prefix('momento')->group(function() {
-	Route::get('/getAll', 'MomentosController@getAll');
-	Route::post('/save/{planoDeAulaId}', 'MomentosController@adicionar');
-	Route::post('/edit/{id}', 'MomentosController@editar');
-});
-
-Route::prefix('recurso')->group(function() {
-	Route::get('/getAll', 'RecursosController@getAll');
-	Route::post('/save', 'RecursosController@adicionar');
-	Route::post('/edit/{id}', 'RecursosController@editar');
-});
